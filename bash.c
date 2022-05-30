94a2254079e8a12594d16e934a6aab83d8df52e0
WaleedButt2 <waleedb403@gmail.com>
2022-05-31 03:21:43 +0500

    asa

--- bash.c
+++ bash.c
@@ -197,7 +197,7 @@ int main(){
         int pid=fork();
         if(pid==0){
             redirect(argv,&argc);
-            printf("%d\n",argc);
+            //printf("%d\n",argc);
             Print(argv,argc);
             if(execvp(argv[0],argv)==-1) printf("poop\n");
             for(int i=0;i<=argc;i++) free(argv[i]);
